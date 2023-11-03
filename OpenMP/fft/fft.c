94be50de686e07f27f13d76f2143ec314b3ed5fd
hellopuza <pluton11a@gmail.com>
2023-11-03 21:09:12 +0300

    fft sse

--- OpenMP/fft/fft.c
+++ OpenMP/fft/fft.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <complex.h>
 #include <omp.h>
-#include <immintrin.h>
+#include <smmintrin.h>
+
 #include "avx_mathfun.h"
 
 #define complex_t complex float
@@ -109,42 +110,39 @@ void fft_parallel_opt(const float* data, complex_t* out, size_t size)
         const float a = 2.0F * PI / m;
         const size_t n = m >> 1;
         #pragma omp parallel for
-        for (size_t j = 0; j < end_j; j += 8)
+        for (size_t j = 0; j < end_j; j += 4)
         {
-            __m256i jj = _mm256_set_epi32(j + 7, j + 6, j + 5, j + 4, j + 3, j + 2, j + 1, j + 0);
-            __m256i jn = _mm256_set_epi32(
-                (j + 7) % n, (j + 6) % n, (j + 5) % n, (j + 4) % n,
-                (j + 3) % n, (j + 2) % n, (j + 1) % n, (j + 0) % n
-            );
-            __m256i i1 = _mm256_sub_epi32(_mm256_slli_epi32(jj, 1), jn);
-            __m256i i2 = _mm256_add_epi32(i1, _mm256_set1_epi32(n));
+            __m128i jj = _mm_set_epi32(j + 3, j + 2, j + 1, j);
+            __m128i jn = _mm_set_epi32((j + 3) % n, (j + 2) % n, (j + 1) % n, j % n);
+            __m128i i1 = _mm_sub_epi32(_mm_slli_epi32(jj, 1), jn);
+            __m128i i2 = _mm_add_epi32(i1, _mm_set1_epi32(n));
 
             #undef I
-            #define R(n,ii) ((float*)&(out[_mm256_extract_epi32((ii),(n))]))[0]
-            #define I(n,ii) ((float*)&(out[_mm256_extract_epi32((ii),(n))]))[1]
+            #define R(n,ii) ((float*)&(out[_mm_extract_epi32((ii),(n))]))[0]
+            #define I(n,ii) ((float*)&(out[_mm_extract_epi32((ii),(n))]))[1]
 
-            __m256 re1 = _mm256_set_ps(R(7,i1), R(6,i1), R(5,i1), R(4,i1), R(3,i1), R(2,i1), R(1,i1), R(0,i1));
-            __m256 re2 = _mm256_set_ps(R(7,i2), R(6,i2), R(5,i2), R(4,i2), R(3,i2), R(2,i2), R(1,i2), R(0,i2));
-            __m256 im1 = _mm256_set_ps(I(7,i1), I(6,i1), I(5,i1), I(4,i1), I(3,i1), I(2,i1), I(1,i1), I(0,i1));
-            __m256 im2 = _mm256_set_ps(I(7,i2), I(6,i2), I(5,i2), I(4,i2), I(3,i2), I(2,i2), I(1,i2), I(0,i2));
+            __m128 re1 = _mm_set_ps(R(3,i1), R(2,i1), R(1,i1), R(0,i1));
+            __m128 re2 = _mm_set_ps(R(3,i2), R(2,i2), R(1,i2), R(0,i2));
+            __m128 im1 = _mm_set_ps(I(3,i1), I(2,i1), I(1,i1), I(0,i1));
+            __m128 im2 = _mm_set_ps(I(3,i2), I(2,i2), I(1,i2), I(0,i2));
 
-            __m256 w = _mm256_mul_ps(_mm256_cvtepi32_ps(jn), _mm256_set1_ps(a));
-            __m256 c = cos256_ps(w);
-            __m256 s = sin256_ps(w);
+            __m256 w = _mm256_castps128_ps256(_mm_mul_ps(_mm_cvtepi32_ps(jn), _mm_set1_ps(a)));
+            __m256 s, c;
+            sincos256_ps(*(__m256*)&w, (__m256*)&s, (__m256*)&c);
 
-            __m256 t = _mm256_sub_ps(_mm256_mul_ps(re2, c), _mm256_mul_ps(im2, s));
-            im2 = _mm256_add_ps(_mm256_mul_ps(re2, s), _mm256_mul_ps(im2, c));
+            __m128 t = _mm_sub_ps(_mm_mul_ps(re2, _mm256_castps256_ps128(c)), _mm_mul_ps(im2, _mm256_castps256_ps128(s)));
+            im2 = _mm_add_ps(_mm_mul_ps(re2, _mm256_castps256_ps128(s)), _mm_mul_ps(im2, _mm256_castps256_ps128(c)));
             re2 = t;
 
-            __m256 x11 = _mm256_add_ps(re1, re2);
-            __m256 x12 = _mm256_add_ps(im1, im2);
-            __m256 x21 = _mm256_sub_ps(re1, re2);
-            __m256 x22 = _mm256_sub_ps(im1, im2);
+            __m128 x11 = _mm_add_ps(re1, re2);
+            __m128 x12 = _mm_add_ps(im1, im2);
+            __m128 x21 = _mm_sub_ps(re1, re2);
+            __m128 x22 = _mm_sub_ps(im1, im2);
 
             #define F(n,xii) ((float*)&xii)[n]
             #define C(n) R(n,i1) = F(n,x11); I(n,i1) = F(n,x12); R(n,i2) = F(n,x21); I(n,i2) = F(n,x22);
 
-            C(0); C(1); C(2); C(3); C(4); C(5); C(6); C(7);
+            C(0); C(1); C(2); C(3);
         }
     }
 }
